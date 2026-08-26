f1e7ae710a8c9b4544638c9aeefcc8f43b398b95
Shookrun <sukranmamedov999@gmail.com>
2026-08-27 03:41:57 +0400

    refactor: remove unnecessary comment from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
             <Navbar />
           </div>
 
-          {/* Content */}
           <main className="page-enter">
             {children}
           </main>
